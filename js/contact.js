9b66db074be940e64af85c573e04b5c35458b33b
Louis Machin <ldhmachin@gmail.com>
2021-08-10 14:43:52 +0100

    Added email google form

--- js/contact.js
+++ js/contact.js
@@ -1,39 +1,8 @@
 const gs_url = "https://script.google.com/macros/library/d/1qvxfPktNTGTKIzWGSJed8Bs5ueUCP27bh3SUq30XhHHnWaPgWDXIxNa-/2";
 
-function sendEmailData( data ) {
-  console.log( 'Sending data' );
+//https://docs.google.com/forms/d/e/1FAIpQLScJGHtqXizArzA4BcRs5jxsdePu69UJGPyGUgk2bV5ryx0GAQ/viewform?usp=pp_url&entry.1176030620=example@example.com
 
-  const XHR = new XMLHttpRequest();
-
-  let urlEncodedData = "",
-      urlEncodedDataPairs = [],
-      name;
-
-  // Turn the data object into an array of URL-encoded key/value pairs.
-  for( name in data ) {
-    urlEncodedDataPairs.push( encodeURIComponent( name ) + '=' + encodeURIComponent( data[name] ) );
-  }
-
-  // Combine the pairs into a single string and replace all %-encoded spaces to
-  // the '+' character; matches the behavior of browser form submissions.
-  urlEncodedData = urlEncodedDataPairs.join( '&' ).replace( /%20/g, '+' );
-
-  // Define what happens on successful data submission
-  XHR.addEventListener( 'load', function(event) {
-    alert( 'Yeah! Data sent and response loaded.' );
-  } );
-
-  // Define what happens in case of error
-  XHR.addEventListener( 'error', function(event) {
-    alert( 'Oops! Something went wrong.' );
-  } );
-
-  // Set up our request
-  XHR.open( 'POST', gs_url);
-
-  // Add the required HTTP header for form data POST requests
-  XHR.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-
-  // Finally, send our data.
-  XHR.send( urlEncodedData );
-}
+$('#gform').on('submit', function(e) {
+    $('#gform *').fadeOut(2000);
+    $('#gform').prepend('Your submission has been processed...');
+});
